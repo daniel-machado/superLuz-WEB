6b901001846d8b7c04275be693a8c05baba273aa
Daniel Machado <daniel7oficial@gmail.com>
2025-05-15 02:26:34 -0300

    ajustando Endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL_DEV; //|| "http://localhost:5000/api"; // URL padrão para desenvolvimento
+const API_URL = import.meta.env.VITE_API_URL; //|| "http://localhost:5000/api"; // URL padrão para desenvolvimento
 
 const api = axios.create({
   baseURL: API_URL,
